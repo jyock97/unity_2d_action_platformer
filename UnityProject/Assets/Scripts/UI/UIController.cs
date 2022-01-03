6bea50469daca5f3850e243807f35974568fd6eb
jyock97 <jyock1997@gmail.com>
2022-01-02 20:58:02 -0600

    Fix Menu Keyboard Interaction

--- UnityProject/Assets/Scripts/UI/UIController.cs
+++ UnityProject/Assets/Scripts/UI/UIController.cs
@@ -1,18 +1,54 @@
 using System;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
+    [SerializeField] private Button startButton;
+    [SerializeField] private Button exitButton;
     [SerializeField] private Sprite startMenuButtonImage;
     [SerializeField] private Sprite startMenuButtonHighlightImage;
 
-    public void SetStartMenuHighLightImage(Button button)
+    private void Start()
+    {
+        startButton.Select();
+    }
+
+    private void Update()
+    {
+        if (EventSystem.current.currentSelectedGameObject == null &&
+            Mathf.Abs(Input.GetAxisRaw("Vertical")) > 0)
+        {
+            startButton.Select();
+        } 
+        SelectDeselectButton(startButton);
+        SelectDeselectButton(exitButton);
+    }
+
+    private void SelectDeselectButton(Button button)
+    {
+        if (EventSystem.current.currentSelectedGameObject == button.gameObject)
+        {
+            SetStartMenuHighLightImage(button);
+        }
+        else
+        {
+            SetStartMenuImage(button);
+        }
+    }
+
+    public void HoverSelect(Button button)
+    {
+        EventSystem.current.SetSelectedGameObject(button.gameObject);
+    }
+
+    private void SetStartMenuHighLightImage(Button button)
     {
         button.image.sprite = startMenuButtonHighlightImage;
     }
-    public void SetStartMenuImage(Button button)
+    private void SetStartMenuImage(Button button)
     {
         button.image.sprite = startMenuButtonImage;
     }
